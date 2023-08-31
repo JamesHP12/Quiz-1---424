eaafe756f00d53954373bd4a41ef77d98d284d42
JamesHP12 <136855151+JamesHP12@users.noreply.github.com>
2023-08-31 17:07:31 -0400

    Add files via upload

---
+++ cmake-firstquiz-plswork.cpp
@@ -0,0 +1,12 @@
+﻿// cmake-firstquiz-plswork.cpp : Defines the entry point for the application.
+//
+
+#include "cmake-firstquiz-plswork.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "James Pifer" << endl;
+	return 0;
+}
